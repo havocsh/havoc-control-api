84a85093f1151ba3c918c345e95bdca252b3d3b1
Tom D'Aquino <tom@havocops.io>
2021-07-13 22:38:26 -0700

    Fixed api_domain_name declaration

--- authorizer/lambda_function.py
+++ authorizer/lambda_function.py
@@ -6,9 +6,10 @@ from authorizer import Login
 def lambda_handler(event, context):
     region = re.search('arn:aws:lambda:([^:]+):.*', context.invoked_function_arn).group(1)
     campaign_id = os.environ['CAMPAIGN_ID']
-    api_domain_name = os.environ['API_DOMAIN_NAME']
-    if not api_domain_name:
+    if 'API_DOMAIN_NAME' not in os.environ:
         api_domain_name = f'{event["apiId"]}.execute-api.{region}.amazonaws.com'
+    else:
+        api_domain_name = os.environ['API_DOMAIN_NAME']
 
     auth = Login(region, campaign_id, api_domain_name, event)
     auth.authorize_keys()
