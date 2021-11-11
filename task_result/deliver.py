751edef16fc1ecd3fa8ef52adffa2e0eaba054c5
Tom D'Aquino <tom@havocops.io>
2021-11-10 22:09:53 -0800

    Extended delay after task termination

--- task_result/deliver.py
+++ task_result/deliver.py
@@ -264,7 +264,7 @@ class Deliver:
                 self.update_domain_entry(task_domain_name, domain_tasks, domain_host_names)
                 self.delete_resource_record_set(hosted_zone, task_host_name, task_domain_name, task_attack_ip)
             self.delete_task_entry()
-            t.sleep(10)
+            t.sleep(20)
         else:
             self.update_task_entry(stime, 'idle', task_end_time)
 
